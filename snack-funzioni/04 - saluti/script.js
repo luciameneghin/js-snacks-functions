975e8a46f51878b3a821b4c121e2ea204bf9720d
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-20 20:06:10 +0100

    feat: exercise 4- saluti

--- snack-funzioni/04 - saluti/script.js
+++ snack-funzioni/04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
-
+const goodMorning = 'Buon Giorno'
 
 // Dichiara la funzione qui.
+const greeting = (hello, myName) => `${hello}, ${myName}!`
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greeting(goodMorning, name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
