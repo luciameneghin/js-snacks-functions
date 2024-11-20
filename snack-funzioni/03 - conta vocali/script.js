e63e52b4435cb59af608fcaf72c29a943a9b5756
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-20 19:55:40 +0100

    feat : exercise 3- conta vocali

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -2,12 +2,41 @@
 
 const word = 'javascript';
 
+const vocals = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
+
+function vocalsNumber(mainWord, aeiou) {
+  let counter = 0;
+
+  const arrayWord = mainWord.split('');
+  console.log(arrayWord)
+
+  for (let i = 0; i < arrayWord.length; i++) {
+    if (aeiou.includes(arrayWord[i])) {
+      counter++
+    }
+  }
+  return counter
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vocalsNumber(word, vocals))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+/*
+Logica:
+- creo array con vocali 
+nella funzione:
+- stringa vuota (counter) per il numero di vocali 
+- trasformo la stringa in array (.split)
+- for i=0; i<word.length; i++
+- if le lettere della parola sono vocali -> counter++
+- return counter 
+
+- console.log (nome funzione(word, array))
+*/
